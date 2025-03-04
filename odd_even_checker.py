3196fd44fdd11b5120bc2e981d32259e8f21c200
TheJMan177 <jalenchall1020@gmail.com>
2025-03-04 16:46:10 -0500

    Add files via upload

---
+++ odd_even_checker.py
@@ -0,0 +1,11 @@
+# List of 15 numbers
+numbers = [3, 8, 15, 22, 7, 10, 33, 40, 13, 28, 5, 16, 21, 12, 30]
+
+# Loop through each number and determine if it's even or odd
+for number in numbers:
+    if number % 2 == 0:
+        print(str(number) + " is even")
+    else:
+        print(str(number) + " is odd")
+
+        
